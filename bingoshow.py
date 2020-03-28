39ca4faec2f1ea138c80c980d393dc15bded3aaa
Bendik Standal <bendik.standal@gmail.com>
2020-03-28 18:27:12 +0100

    in the middle of changing to general implementation with more possible boards.

--- bingoshow.py
+++ bingoshow.py
@@ -13,13 +13,11 @@ import numpy as np
 from screeninfo import get_monitors
 
 m = get_monitors()
-print(m)
 SCREEN_WIDTH = m[0].width
-SCREEN_HEIGHT = m[0].height
-SCREEN_MARGIN_VER = SCREEN_HEIGHT//6
-SCREEN_MARGIN_HOR = SCREEN_WIDTH//6
-WIDTH, HEIGHT = SCREEN_MARGIN_VER//2, SCREEN_MARGIN_VER//2
+SCREEN_HEIGHT = m[0].height-(m[0].height//6)
 SCREEN_TITLE = "BINGO"
+
+
 
 
 class MyGame(arcade.Window):
@@ -37,12 +35,21 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.BLACK)
 
         self.bingo_boards = bingocheck.test_board_two
-        self.bingo_list = bingocheck.test_list
+        self.bingo_list = []
         self.nb_boards = len(self.bingo_boards)
-        self.rows = len(self.bingo_boards[0])
-        self.columns = len(self.bingo_boards[0, 0])
-        # If you have sprite lists, you should create them here,
-        # and set them to None
+        self.rows_per_board = len(self.bingo_boards[0])
+        self.columns_per_board = len(self.bingo_boards[0, 0])
+
+        self.columns_of_boards = 5 if self.nb_boards >= 5 else self.nb_boards
+        self.rows_of_boards = 1 + self.nb_boards // 5
+
+        self.screen_margin_vertical = SCREEN_HEIGHT//12
+        self.screen_margin_horizontal = SCREEN_WIDTH//10
+
+        self.new_numbers_list_height = SCREEN_HEIGHT//4
+        self.board_size_x = (SCREEN_WIDTH - (1 + self.columns_of_boards) * self.screen_margin_horizontal) // (self.columns_of_boards)
+
+        self.cell_width, self.cell_height = self.board_size_x//self.rows_per_board, self.board_size_x//self.rows_per_board
 
     def setup(self):
         # Create your sprites and sprite lists here
@@ -52,12 +59,9 @@ class MyGame(arcade.Window):
         """
         Render the screen.
         """
-
-        # This command should happen before we start drawing. It will clear
-        # the screen to the background color, and erase what we drew last frame.
         arcade.start_render()
 
-        # Call draw() on all your sprite lists below
+        
         for i in range(len(self.bingo_boards)):
             self.draw_board(self.bingo_boards[i], i)
         self.draw_list(self.bingo_list)
@@ -84,32 +88,33 @@ class MyGame(arcade.Window):
 
 
     def draw_board(self, board, i):
-        for row in range(self.rows):
-            for column in range(self.columns):
+        for row in range(self.rows_per_board):
+            for column in range(self.columns_per_board):
                 data = board[row, column]
-                x = (i * self.columns * WIDTH * 2) + (WIDTH) * column + SCREEN_MARGIN_HOR + WIDTH // 2
-                y = (HEIGHT) * row + SCREEN_MARGIN_VER//2 + HEIGHT // 2
+                x = (1 + i % self.columns_of_boards) * self.screen_margin_horizontal + (i * self.board_size_x) + (self.cell_width * column)
+                y = (i // 5 + 1) * self.screen_margin_vertical + ((i//5)  * self.board_size_x) + (self.cell_width * row)
+                if i == 0:
+                    print(y)
 
                 if data in self.bingo_list:
-                    arcade.draw_rectangle_filled(x, y, WIDTH, HEIGHT, arcade.color.GREEN)
-                    arcade.draw_text(str(data), x, y, arcade.color.BLACK, 30, align="center", anchor_x="center", anchor_y="center", bold=True)
+                    arcade.draw_rectangle_filled(x, y, self.cell_width, self.cell_height, arcade.color.GREEN)
+                    arcade.draw_text(str(data), x, y, arcade.color.BLACK, self.cell_width//2, align="center", anchor_x="center", anchor_y="center", bold=True)
                 else:
-                    arcade.draw_rectangle_outline(x, y, WIDTH, HEIGHT, arcade.color.WHITE)
-                    arcade.draw_text(str(data), x, y, arcade.color.WHITE, 30, align="center", anchor_x="center", anchor_y="center", bold=True)
+                    arcade.draw_rectangle_outline(x, y, self.cell_width, self.cell_height, arcade.color.WHITE)
+                    arcade.draw_text(str(data), x, y, arcade.color.WHITE, self.cell_width//2, align="center", anchor_x="center", anchor_y="center", bold=True)
                 
     def draw_list(self, liste):
         for i in range(len(liste)):
-            x = SCREEN_MARGIN_HOR//2 + i%20 * WIDTH
-            y = SCREEN_HEIGHT - SCREEN_MARGIN_VER//2 - i//20*WIDTH
-            arcade.draw_rectangle_outline(x, y, WIDTH, HEIGHT, arcade.color.WHITE)
-            arcade.draw_text(str(liste[i]), x, y, arcade.color.WHITE, 30, align="center", anchor_x="center", anchor_y="center", bold=True)
+            x = self.screen_margin_horizontal//2 + i%20 * self.cell_width
+            y = SCREEN_HEIGHT - self.screen_margin_vertical//2 - i//20*self.cell_width
+            arcade.draw_rectangle_outline(x, y, self.cell_width, self.cell_height, arcade.color.WHITE)
+            arcade.draw_text(str(liste[i]), x, y, arcade.color.WHITE, self.cell_width//2, align="center", anchor_x="center", anchor_y="center", bold=True)
 
 def main():
     """ Main method """
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     game.setup()
     arcade.run()
-    i = input("test")
 
 
 if __name__ == "__main__":
